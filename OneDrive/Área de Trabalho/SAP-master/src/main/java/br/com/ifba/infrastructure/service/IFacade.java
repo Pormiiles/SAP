86c76bb30610631d91b6499a30ee647fbd227cd8
Andre <andrelima.santos2370@gmail.com>
2023-10-27 18:16:41 -0300

    Base do projeto e telas de teste para o banco de dados

---
+++ OneDrive/Área de Trabalho/SAP-master/src/main/java/br/com/ifba/infrastructure/service/IFacade.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.infrastructure.service;
+
+
+import br.com.ifba.teste.model.Teste;
+
+import java.util.List;
+
+/**
+ *
+ * @author clebinho
+ */
+public interface IFacade {
+    public abstract void saveTeste(Teste teste);
+
+    
+}
