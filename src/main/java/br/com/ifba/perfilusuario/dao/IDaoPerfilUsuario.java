2dd904e4ea16cf5ed6a6c99fbfd96cbf58bb89d3
Mendes-Matheus <msmmendes211@gmail.com>
2023-10-31 13:49:44 -0300

    Alterações na indentação da classe PerfilUsuario e alterações de nomenclaturas na classe IDaoPerfilUsuario

--- src/main/java/br/com/ifba/perfilusuario/dao/IDaoPerfilUsuario.java
+++ src/main/java/br/com/ifba/perfilusuario/dao/IDaoPerfilUsuario.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Repository;
  * @author ph715
  */
 @Repository
-public interface IPerfilUsuarioDao extends JpaRepository<PerfilUsuario, Long> {
+public interface IDaoPerfilUsuario extends JpaRepository<PerfilUsuario, Long> {
     
     public List<PerfilUsuario> findByNome(String nome);
     public boolean existsByNome(String nome);
